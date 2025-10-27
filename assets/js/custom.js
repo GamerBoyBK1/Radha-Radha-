9a9f1e64054c8967179a63ac2f3f3ef9decec823
GamerBoyBK1 <161742908+GamerBoyBK1@users.noreply.github.com>
2025-10-27 15:00:59 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,20 +1,7 @@
 $(function () {
-    let lastScroll = 0;
-    const $nav = $(".fixed-top");
+    const $nav = $(".navbar");
 
     $(document).on("scroll", function () {
-        let currentScroll = $(this).scrollTop();
-
-        // Navbar toggle
-        $nav.toggleClass("scrolled", currentScroll > $nav.height());
-
-        // Add smooth feel when scrolling down/up
-        if (currentScroll > lastScroll) {
-            $nav.addClass("scrolling-down").removeClass("scrolling-up");
-        } else {
-            $nav.addClass("scrolling-up").removeClass("scrolling-down");
-        }
-
-        lastScroll = currentScroll;
+        $nav.toggleClass("scrolled", window.scrollY > 50);
     });
 });
